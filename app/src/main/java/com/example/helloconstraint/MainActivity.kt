1d2d8a61feb11f7c65adb964df7c0b1222cd87b9
seropp <ts.seropps@gmail.com>
2022-04-07 17:09:55 +0300

    change backgrountd for btn

--- app/src/main/java/com/example/helloconstraint/MainActivity.kt
+++ app/src/main/java/com/example/helloconstraint/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.helloconstraint
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.helloconstraint.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
+
+class MainActivity : AppCompatActivity() {
 
     private lateinit var vm: MainViewModel
     private lateinit var binding: ActivityMainBinding
@@ -20,12 +25,24 @@ class MainActivity : AppCompatActivity(){
         vm = ViewModelProvider(this)[MainViewModel::class.java]
         vm.getLiveData().observe(this, Observer {
             binding.textView.text = it.toString()
-            if (it%2 != 0)binding.textView.setBackgroundResource(R.color.purple_500)
-            else binding.textView.setBackgroundResource(R.color.teal_700)
+//            if (it%2 != 0)binding.textView.setBackgroundResource(R.color.purple_500)
+            if (it % 2 != 0) binding.btn2.background.setTint(ContextCompat.getColor(this, R.color.teal_200))
+            else binding.btn2.background.setTint(ContextCompat.getColor(this, R.color.green))
+
+            if (it != 0) {
+                binding.btn3.isEnabled = true
+                binding.btn3.background.setTint(ContextCompat.getColor(this, R.color.teal_700))
+            }
+            else {
+                binding.btn3.background.setTint(ContextCompat.getColor(this, R.color.gray))
+                binding.btn3.isEnabled = false
+            }
+
+
         })
-    
+
         binding.btn1.setOnClickListener {
-            Toast.makeText(this,"First bnt", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "First bnt", Toast.LENGTH_SHORT).show()
         }
         binding.btn2.setOnClickListener {
             vm.setData()
